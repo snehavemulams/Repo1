6313e2d25b3090dc34ec599f7264062cf7e0a6e2
snehavemulams <47413781+snehavemulams@users.noreply.github.com>
2019-02-07 14:12:25 +0530

    Create Hello.cs

---
+++ Hello.cs
@@ -0,0 +1,12 @@
+using System;
+namespace HelloWorld
+{
+    class Hello 
+    {
+        static void Main() 
+        {
+            Console.WriteLine("Hello World from github project!");
+           
+        }
+    }
+}
